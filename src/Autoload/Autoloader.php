863fef3fd160328b3623f242e6a362a207e7088a
amadoudiall <68468761+amadoudiall@users.noreply.github.com>
2022-03-11 17:18:58 +0000

    Correction de l'autoloader 2

--- src/Autoload/Autoloader.php
+++ src/Autoload/Autoloader.php
@@ -15,6 +15,7 @@ class Autoloader
         if (strpos($class, __NAMESPACE__ . '\\') === 0) {
 
             $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+            //$class = str_replace('\\', '/', $class);
             require('../src/' . $class . '.php');
         }
     }
@@ -29,6 +30,7 @@ class Autoloader
         if (strpos($class, __NAMESPACE__ . '\\') === 0) {
 
             $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+            //$class = str_replace('\\', '/', $class);
             require('./src/' . $class . '.php');
         }
     }
